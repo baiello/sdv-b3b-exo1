14d06805bd36f3edcc84d32f79c51023a406bbe8
Brice AIELLO <brice.aiello.fr@gmail.com>
2024-10-14 14:26:44 +0200

    Add loader + event handler

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ import { createRestaurant } from "./factories/restaurant.js";
 
 const dataUrl = 'https://gist.githubusercontent.com/baiello/a42c640d3bd6a52d965b29e3e0de6db7/raw/01f153c8c1099605476721b5326df0d533b283c7/restaurants.json'
 
+const myEvent = new Event('Restaurants fetched and displayed');
+
 export function getData() {
     return fetch(dataUrl)
         .then( response => response.json() )
@@ -19,5 +21,11 @@ getData().then(
             restaurantsContainer.append(restaurant.createCard());
         }
 
+        document.dispatchEvent(myEvent);
     }
 );
+
+document.addEventListener('Restaurants fetched and displayed', () => {
+    const loaderElement = document.getElementById('restaurants-loader');
+    loaderElement.remove();
+});
